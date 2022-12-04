f89b914db41240d39ffe392dbc2c9dc17bb63434
AdrianaDobre <adriana.dobre5@s.unibuc.ro>
2022-12-04 17:09:57 +0200

    created login page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
+import {Credentials} from "../../interfaces/credentials";
+import {AppRoutes} from "../../app-routing.module";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,27 @@ import {Router} from "@angular/router";
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit{
-  constructor() {}
+  public user: Credentials = {
+    email: '',
+    password: '',
+  };
+  public error: string | boolean = false;
+
+  constructor(private router:Router) {}
 
   ngOnInit(): void {
   }
 
+  doLogin() {
+    this.error = false;
+    if (this.user.email && this.user.password) {
+      console.log(this.user)
+    } else {
+      this.error = 'Enter your email and password';
+    }
+  }
+
+  goToRegister() {
+    this.router.navigate([AppRoutes.REGISTER]);
+  }
 }
