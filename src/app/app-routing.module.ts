39200f3e7f3fc406fcd0f2d7c0e1f6ba34eb5136
AdrianaDobre <adriana.dobre5@s.unibuc.ro>
2022-12-06 16:42:46 +0200

    created books page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,17 +3,21 @@ import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from "./pages/login/login.component";
 import {HomeComponent} from "./pages/home/home.component";
 import {RegisterComponent} from "./pages/register/register.component";
+import {BooksComponent} from "./pages/books/books.component";
 
 export enum AppRoutes{
   HOME='home',
   LOGIN='login',
-  REGISTER='register'
+  REGISTER='register',
+  BOOKS = 'books'
+
 }
 const routes: Routes = [
   {path:'', redirectTo: AppRoutes.HOME, pathMatch:'full'},
   {path: AppRoutes.HOME,component: HomeComponent},
   {path: AppRoutes.LOGIN, component: LoginComponent},
-  {path: AppRoutes.REGISTER, component: RegisterComponent}
+  {path: AppRoutes.REGISTER, component: RegisterComponent},
+  {path: AppRoutes.BOOKS, component: BooksComponent}
 ];
 
 @NgModule({
---
+++ src/app/pages/books/books.component.ts
@@ -0,0 +1,28 @@
+import {Component, OnInit} from '@angular/core';
+import {AppRoutes} from "../../app-routing.module";
+import {Router} from "@angular/router";
+import {Book} from "../../interfaces/book";
+import {BooksService} from "../../services/books/books.service";
+
+@Component({
+  selector: 'app-books',
+  templateUrl: './books.component.html',
+  styleUrls: ['./books.component.css'],
+  providers:[BooksService]
+})
+export class BooksComponent implements OnInit{
+  public books!: Array<Book>;
+
+  constructor(private router:Router, private booksService: BooksService) {}
+
+  ngOnInit(): void {
+    this.booksService.getBooks().subscribe(result => {
+      this.books = result;
+    })
+  }
+
+  onHomeClick() {
+    this.router.navigate([AppRoutes.HOME]);
+  }
+
+}
